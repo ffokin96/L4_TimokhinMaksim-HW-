2b440fdd9eb4b3c3a93d6930af7875453946d426
MaximTimokhin <ffoka96@icloud.com>
2020-07-29 10:50:07 +0300

    4HW(1)

--- L4_TimokhinMaksim(HW)/main.swift
+++ L4_TimokhinMaksim(HW)/main.swift
@@ -1,12 +1,152 @@
 //
 //  main.swift
-//  L4_TimokhinMaksim(HW)
+//  L3_TimokhinMaksim(HW)
 //
-//  Created by Максим Тимохин on 29.07.2020.
+//  Created by Максим Тимохин on 24.07.2020.
 //  Copyright © 2020 Максим Тимохин. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+enum CarAction {
+    case turnEngineOn
+    case turnEngineOff
+    case openWindow
+    case closeWindow
+}
+
+enum SportCarAction {
+    case spolerUp
+    case spolerDown
+    case speedUp
+    case speedDowm
+}
+
+enum TrunkCarAction {
+    case truckLoad
+    case truckUnLoad
+}
+
+
+
+class Car {
+    var color: Int
+    var wheelCount: Int
+    var isEngineOn: Bool = false
+    var isWindowOn: Bool = true
+    
+    
+    
+    init (color: Int, wheelCount: Int) {
+        self.color = color
+        self.wheelCount = wheelCount
+    }
+    
+    func carAction(action: CarAction) {
+        switch action {
+        case .turnEngineOff:
+            guard isEngineOn else {
+                print("Двигатель уже включен")
+                return
+            }
+            isEngineOn = false
+            
+        case .turnEngineOn:
+            guard !isEngineOn else {
+                print("Двигатель уже выключен")
+                return
+            }
+            isEngineOn = true
+            
+        case .openWindow:
+            guard !isWindowOn else {
+                print("Окна уже открыты")
+                return
+            }
+            isWindowOn = false
+            
+        case .closeWindow:
+            guard isWindowOn else {
+                print("Окна уже закрыты")
+                return
+            }
+            isWindowOn = true
+        }
+    }
+}
+class SportCar: Car {
+    var isSpolerOn: Bool = true
+    let maxSpeedLimit: Int = 252849
+    var speed: Int = 0
+    
+    override func carAction(action: CarAction) {
+        super.carAction(action: action)
+    }
+    
+    func sportCarAction(action: SportCarAction){
+        if action == .speedUp {
+            speed = min(speed + 1, maxSpeedLimit)
+            
+        } else if action == .speedDowm {
+            speed = max(speed - 1, 0)
+        }
+        
+        switch action {
+        case .spolerUp:
+            guard isSpolerOn else {
+                print("Сполер уже поднят")
+                return
+            }
+            
+            isSpolerOn = true
+        case .spolerDown:
+            guard !isSpolerOn else {
+                print("Сполер уже опущен")
+                return
+            }
+            isSpolerOn = false
+            
+        default:
+            print("ХЗ")
+        }
+    }
+}
+
+class TrunkCar: Car {
+    var trunkCappacity: Int
+    var trunkIn: Int = 0
+    
+    init(cappacity: Int, wheelCount: Int, color: Int) {
+        trunkCappacity = cappacity
+        super.init(color: color, wheelCount: wheelCount)
+    }
+    
+    override func carAction(action: CarAction) {
+        super.carAction(action: action)
+    }
+    func truckCarAction(action: TrunkCarAction) {
+        
+        if action == .truckLoad {
+            trunkIn = min(trunkIn + 1, trunkCappacity )
+        }
+        else if action == .truckUnLoad {
+            trunkIn = max(trunkIn - 1, 0)
+            
+        }
+    }
+}
+
+let truck = TrunkCar(cappacity: 100, wheelCount: 16, color: 0x000000)
+truck.carAction(action: .turnEngineOn)
+truck.truckCarAction(action: .truckLoad)
+truck.carAction(action: .openWindow)
+
+let sportCar = SportCar(color: 0x738678, wheelCount: 4)
+sportCar.sportCarAction(action: .speedUp)
+sportCar.sportCarAction(action: .spolerDown)
+
+print("Truck: в багажнике \(truck.trunkIn)кг, двигатель включен \(truck.isEngineOn)")
+print("SportCar: окна \(sportCar.isWindowOn), сполер \(sportCar.isSpolerOn), скорость \(sportCar.speed)")
+
+print("Cпасибо за внимание")
 
